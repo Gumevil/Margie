58dd26e58928023bb9a0969f6ba01eedf04a17dc
Gumevil <genedixon@rocketmail.com>
2014-10-13 15:22:55 -0400

    changed case of myName
    changed case of myName

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var green = [75, 100, 40];
 var blue = [196, 77, 55];
 var purple = [280, 50, 60];
 
-var myName = "IWTKYAO";
+var myName = "iwtkyao";
 //var myName = prompt("Type a name");
 var letterColors = [red, green, blue, orange, purple];
 if(10 > 3) {
