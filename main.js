2267956ac957accab1c2aa92ade9a5aed46dcc5e
Gumevil <genedixon@rocketmail.com>
2014-11-16 12:00:40 -0500

    changed so that user inputs name

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ var green = [75, 100, 40];
 var blue = [196, 77, 55];
 var purple = [280, 50, 60];
 
-var myName = "Margie";
-//var myName = prompt("Type a name");
+//var myName = "Margie";
+var myName = prompt("Type a name");
 var letterColors = [red, green, blue, orange, purple];
 if(10 > 3) {
     bubbleShape = "circle";
